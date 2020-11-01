04b1591af93d763e8cddf2190534536a8ec4ed6d
bentkowskiw <bentkowski@codogrosza.pl>
2020-11-01 21:17:31 +0100

    Product cleanup

--- src/main/java/io/bentkowski/store/controller/ProductRestController.java
+++ src/main/java/io/bentkowski/store/controller/ProductRestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-public class ProductRestController implements ProductRestApi {
+public class ProductRestController {
 
     @Autowired
     private ProductService productService;
@@ -22,25 +22,25 @@ public class ProductRestController implements ProductRestApi {
         this.productService = productService;
     }
 
-    @Override
+
     @PostMapping("/products")
     public ProductDto addProduct(@RequestBody ProductDto product) {
         return productService.save(product);
     }
 
-    @Override
+
     @PutMapping("/products/{SKU}")
     public ProductDto updateProduct(@RequestBody ProductDto product, @PathVariable String SKU) {
         return productService.updateProduct(product, SKU);
     }
 
-    @Override
+
     @GetMapping("/products")
     public Iterable<ProductDto> findProducts(@RequestParam(required = false) String offset, @RequestParam(required = false) String limit) {
         return productService.findAll(offset, limit);
     }
 
-    @Override
+
     @DeleteMapping("/products/{sku}")
     public void deleteProduct(@PathVariable String sku) {
         productService.deleteById(sku);
